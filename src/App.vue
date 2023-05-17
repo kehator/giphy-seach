55cc24119c3081613685a1055969c5c760dbd232
Dawid Moroch <dawid.moroch@gmail.com>
2023-05-17 12:03:05 +0000

    feat(tailwindcss): add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+    import Gifs from './components/Gifs.vue'
     import { fetchGifs } from './composables/api.ts'
 
     async function gifs () {
@@ -9,7 +10,11 @@
 </script>
 
 <template>
-    <div>
-        test
+    <div class="w-full">
+        <div class="flex w-full p-4 justify-center">input</div>
+
+        <div class="flex w-full p-4">
+            <Gifs />
+        </div>
     </div>
 </template>
